80bea836c8e8d48a2e9d93b67533a9755c683f51
Jay <ifiwtbsiwbs@gmail.com>
2020-02-13 01:23:55 +0800

    design index

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,9 @@
 module ProductsHelper
+  def image_check(product)
+    if product.images.attached?
+      image_tag product.images.first.variant(resize_to_limit: [300, 300])
+    else
+      image_tag "http://fakeimg.pl/300x250"
+    end
+  end
 end
